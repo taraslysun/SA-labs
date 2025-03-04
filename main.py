211cc115521e5325c817ff3767bf079d4766d1b7
taraslysun <taraslysun2@gmail.com>
2025-03-04 22:52:00 +0200

    added source file for running data to map

---
+++ main.py
@@ -0,0 +1,13 @@
+import hazelcast
+
+if __name__ == "__main__":
+  client = hazelcast.HazelcastClient(
+  cluster_name="hello-world", 
+  ) 
+
+  # Create a Distributed Map in the cluster
+  map = client.get_map("my-distributed-map").blocking() 
+
+  
+  map.put_all({ i:"hello"+str(i) for i in range(1000) })
+  
